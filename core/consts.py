5b688e623b18761ddfd80eeef719c8fb7482e631
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-23 15:15:16 +0100

    added main Clock + delta time system

--- core/consts.py
+++ core/consts.py
@@ -1,4 +1,10 @@
 class WindowConsts:
-    SCREEN_WIDTH = 300
-    SCREEN_HEIGHT = 250
+    SCREEN_WIDTH = 720
+    SCREEN_HEIGHT = 480
     CAPTION_TEXT = "HawkGame"
+
+    FPS_LIMIT = 120
+
+
+class GameConsts:
+    pass
--- core/game.py
+++ core/game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import time
 from core.consts import WindowConsts
 from core.render.window import Window
 
@@ -8,12 +9,21 @@ class Game:
         self.window = Window(WindowConsts.SCREEN_WIDTH, WindowConsts.SCREEN_HEIGHT, WindowConsts.CAPTION_TEXT)
         self.is_running = False
 
+        self.clock = pg.time.Clock()
+        self.delta_time = 0
+        self.prev_time = 0
+
     def init(self):
         self.is_running = True
         self.window.init()
 
     def mainloop(self):
+        self.prev_time = time.time()
+
         while self.is_running:
+            self.clock.tick(WindowConsts.FPS_LIMIT)
+
+            self.calculate_delta_time()
             self.handle_events(pg.event.get())
 
             self.window.update()
@@ -24,6 +34,12 @@ class Game:
         for event in events:
             if event.type == pg.QUIT:
                 self.is_running = False
+
+    def calculate_delta_time(self):
+        now = time.time()
+
+        self.delta_time = now - self.prev_time
+        self.prev_time = now
 
     def run(self):
         self.init()

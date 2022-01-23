17aea3902393142d4c98b5c061c0cfebbb204edf
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-24 00:11:24 +0100

    tweaked getting player position

--- game_modules/entities/player.py
+++ game_modules/entities/player.py
@@ -2,7 +2,6 @@ from game_modules.entities.entity_base import EntityBase
 from core.utils import vectors_utils
 import os
 import pygame as pg
-import copy
 
 
 class Player(EntityBase):
@@ -11,7 +10,6 @@ class Player(EntityBase):
 
         self.model = None
         self.rect = None
-        self.position = [0, 0]
 
         self.init_player_model()
 
@@ -21,16 +19,10 @@ class Player(EntityBase):
         self.model = pg.image.load(os.path.join(os.path.join("graphic", "player"), "player.png"))
         self.model = pg.transform.scale(self.model, (50, 50))
 
-        self.position = [self.model.get_rect().centerx, self.model.get_rect().centery]
-
-        # For Testing
-        self.position[0] += 100
-        self.position[1] += 100
-
-        self.rect = self.model.get_rect(center=self.position)
+        self.rect = self.model.get_rect()
 
     def get_position(self):
-        return self.position
+        return [self.model.get_rect().centerx, self.model.get_rect().centery]
 
     def set_orientation(self, dest_vector):
         self.orientation_diff = vectors_utils.get_degrees_between_vectors(dest_vector, self.get_position())

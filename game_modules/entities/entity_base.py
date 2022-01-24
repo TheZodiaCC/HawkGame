4ffb6ea7d09c8c354aa788db2674a6551c0d15a8
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-24 16:28:41 +0100

    EntityBase class refactoring

--- game_modules/entities/entity_base.py
+++ game_modules/entities/entity_base.py
@@ -1,3 +1,21 @@
+from core.utils import vectors_utils
+
+
 class EntityBase:
     def __init__(self):
+        self.model = None
+        self.rect = None
+        self.position = [0, 0]
+
+        self.init_entity_model()
+
+        self.orientation_diff = 0
+
+    def init_entity_model(self):
         pass
+
+    def get_position(self):
+        return self.position
+
+    def set_orientation(self, dest_vector):
+        self.orientation_diff = vectors_utils.get_degrees_between_vectors(dest_vector, self.get_position())
--- game_modules/entities/player.py
+++ game_modules/entities/player.py
@@ -1,5 +1,4 @@
 from game_modules.entities.entity_base import EntityBase
-from core.utils import vectors_utils
 import os
 import pygame as pg
 
@@ -12,20 +11,12 @@ class Player(EntityBase):
         self.rect = None
         self.position = [0, 0]
 
-        self.init_player_model()
+        self.init_entity_model()
 
-        self.orientation_diff = 0
-
-    def init_player_model(self):
+    def init_entity_model(self):
         self.model = pg.image.load(os.path.join(os.path.join("graphic", "player"), "player.png"))
         self.model = pg.transform.scale(self.model, (50, 50))
 
         self.rect = self.model.get_rect()
 
         self.position = [self.position[0] - self.model.get_width() / 2, self.position[1] - self.model.get_height() / 2]
-
-    def get_position(self):
-        return self.position
-
-    def set_orientation(self, dest_vector):
-        self.orientation_diff = vectors_utils.get_degrees_between_vectors(dest_vector, self.get_position())
